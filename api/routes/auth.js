90e53402ccf675db1978562f9fafc7b2b215cbe3
Rajesh295-dev <{rajeshgautam766@yahoo.com}+{Rajesh295-dev}@users.noreply.github.com>
2024-02-07 23:28:32 -0500

    added categories  items 🍏

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -8,62 +8,48 @@ const bcrypt = require("bcrypt");
 
 //LOGIN
 
-// router.post("/login", async (req, res) => {
-//     console.log("Request login Body:", req.body);
-//     const inputPassword = req.body.password;
-
-//     try {
-//         const user = await User.findOne({ username: req.body.username });
-
-//         if (!user) {
-//             console.log("User not found");
-//             res.status(400).json("Wrong credentials!");
-//             return;
-//         }
-
-//         const storedHashedPassword = user.password;
-
-//         // Use bcrypt.compare to compare the plain text password with the stored hashed password
-//         bcrypt.compare(inputPassword, storedHashedPassword, (err, result) => {
-//             if (err) {
-//                 console.error("Error during password validation:", err);
-//                 res.status(500).json("Internal Server Error");
-//                 return;
-//             }
-
-//             if (!result) {
-//                 console.log("Invalid password");
-//                 res.status(400).json("Wrong credentials!");
-//                 return;
-//             }
-
-//             const { password, ...others } = user._doc;
-//             console.log("Login successful");
-//             res.status(200).json(others);
-//         });
-
-//     } catch (err) {
-//         console.error("Error during login:", err);
-//         res.status(500).json("Internal Server Error");
-//     }
-
-// });
-
-//LOGIN
 router.post("/login", async (req, res) => {
+    console.log("Request login Body:", req.body);
+    const inputPassword = req.body.password;
+
     try {
         const user = await User.findOne({ username: req.body.username });
-        !user && res.status(400).json("Wrong credentials!");
 
-        const validated = await bcrypt.compare(req.body.password, user.password);
-        !validated && res.status(400).json("Wrong credentials!");
+        if (!user) {
+            console.log("User not found");
+            res.status(400).json("Wrong credentials!");
+            return;
+        }
 
-        const { password, ...others } = user._doc;
-        res.status(200).json(others);
+        const storedHashedPassword = user.password;
+        console.log("stored hashed password:", user.password);
+
+        // Use bcrypt.compare to compare the plain text password with the stored hashed password
+        bcrypt.compare(inputPassword, storedHashedPassword, (err, result) => {
+            if (err) {
+                console.error("Error during password validation:", err);
+                res.status(500).json("Internal Server Error");
+                return;
+            }
+
+            if (!result) {
+                console.log("Invalid password");
+                res.status(400).json("Wrong credentials!");
+                return;
+            }
+
+            const { password, ...others } = user._doc;
+            console.log("Login successful");
+            res.status(200).json(others);
+        });
+
     } catch (err) {
-        res.status(500).json(err);
+        console.error("Error during login:", err);
+        res.status(500).json("Internal Server Error");
     }
+
 });
+
 
 
 
