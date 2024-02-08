f066bcafda77b0e02e73ec46ac79944f75a637f7
Rajesh295-dev <{rajeshgautam766@yahoo.com}+{Rajesh295-dev}@users.noreply.github.com>
2024-02-07 23:38:53 -0500

    added categories  items

--- api/models/User.js
+++ api/models/User.js
@@ -1,57 +1,86 @@
-const { Schema, model } = require("mongoose")
+// const { Schema, model } = require("mongoose")
 
 
-const bcrypt = require('bcrypt')
+// const bcrypt = require('bcrypt')
 
-const UserSchema = new Schema({
+// const UserSchema = new Schema({
 
-    username: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    password: {
-        type: String,
-        required: true,
-    },
+//     username: {
+//         type: String,
+//         required: true,
+//         unique: true
+//     },
+//     password: {
+//         type: String,
+//         required: true,
+//     },
 
-    email: {
-        type: String,
-        required: true,
+//     email: {
+//         type: String,
+//         required: true,
 
-    },
+//     },
 
 
-    profilePic: {
-        type: String,
-        default: "default.png",
-    },
+//     profilePic: {
+//         type: String,
+//         default: "default.png",
+//     },
 
-},
+// },
 
+//     {
+//         timestamps: true
+//     }
+
+// );
+
+// UserSchema.pre("save", async function (next) {
+//     if (this.isNew || this.isModified("password")) {
+//         const saltRounds = 10;
+//         this.password = await bcrypt.hash(this.password, saltRounds);
+//     }
+//     next();
+// });
+
+// UserSchema.methods.isCorrectPassword = async function (password) {
+//     return bcrypt.compare(password, this.password);
+// };
+
+
+// UserSchema.methods.lastUpdatedDate = function () {
+//     this.lastUpdated = Date.now();
+//     return this.lastUpdated;
+// };
+// const User = model("User", UserSchema);
+
+// module.exports = User;
+
+
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema(
     {
-        timestamps: true
-    }
-
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+        profilePic: {
+            type: String,
+            default: "",
+        },
+    },
+    { timestamps: true }
 );
 
-UserSchema.pre("save", async function (next) {
-    if (this.isNew || this.isModified("password")) {
-        const saltRounds = 10;
-        this.password = await bcrypt.hash(this.password, saltRounds);
-    }
-    next();
-});
-
-UserSchema.methods.isCorrectPassword = async function (password) {
-    return bcrypt.compare(password, this.password);
-};
-
-
-UserSchema.methods.lastUpdatedDate = function () {
-    this.lastUpdated = Date.now();
-    return this.lastUpdated;
-};
-const User = model("User", UserSchema);
-
-module.exports = User;
+module.exports = mongoose.model("User", UserSchema);
